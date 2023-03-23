01761bd86e2cffecc5e2abecf7060398396f9943
moisesdurandelrazo <moises6737@gmail.com>
2023-03-23 13:32:03 -0600

    node js

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 /** @format */
 
 import express from "express";
+import usersRoute from "./users";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("hola mundo");
-});
+app.use(express.json());
+app.use("/", usersRoute);
 
 app.listen(3000, () => console.log("Escuchando"));
+
+//curl --header "Content-Type: application/json" --request POST --data '{"name": "Hola mundo"}' http://localhost:3000/
---
+++ users/index.ts
@@ -0,0 +1,21 @@
+/** @format */
+import { Router } from "express";
+
+const router = Router();
+
+type Usuario = {
+  id: string;
+  name: string;
+};
+
+router.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+router.post("/", (req, res) => {
+  const { name } = req.body as Usuario;
+  console.log(name);
+
+  res.send("ok");
+});
+
+export default router;
